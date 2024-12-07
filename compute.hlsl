4d134091f957b3b307efa94459aa8c0dcacc43f0
M Reza Dwi Prasetiawan <Mrezadwiprasetiawan@github.com>
2024-12-07 21:47:00 +0700

    some fix:v

--- compute.hlsl
+++ compute.hlsl
@@ -3,7 +3,7 @@
 [[vk::binding(1, 0)]] RWStructuredBuffer<float> OutBuffer;
 
 [numthreads(WIDTH, 1, 1)]
-int collats(int input);
+
 void main(uint3 DTid : SV_DispatchThreadID)
 {
   //hanya reverse index sebagai contoh
